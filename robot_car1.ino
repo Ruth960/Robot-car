72cb4dc6c286c7ba9546b3dfc233e4a30190df65
Ruth Wanjiru Mwaniki <75331672+Ruth960@users.noreply.github.com>
2025-08-20 10:47:41 +0300

    Update robot_car1.ino

--- robot_car1.ino
+++ robot_car1.ino
@@ -1,63 +1,51 @@
-#define pin1 2
-#define pin2 3
-#define pin3 4
-#define pin4 5
+// Motor driver pin mapping
+const int EN1 = 9;   // L293D pin 1 (Motor 1 enable)
+const int EN2 = 10;  // L293D pin 9 (Motor 2 enable)
+const int IN1 = 2;   // L293D pin 2
+const int IN2 = 3;   // L293D pin 7
+const int IN3 = 4;   // L293D pin 10
+const int IN4 = 5;   // L293D pin 15
 
-void setup()
-{
-  pinMode(pin1,OUTPUT);
-  pinMode(pin2,OUTPUT);
-  pinMode(pin3,OUTPUT);
-  pinMode(pin4,OUTPUT);
-  
+void setup() {
+  // Set all pins as outputs
+  pinMode(EN1, OUTPUT);
+  pinMode(EN2, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Enable both motors at full speed
+  digitalWrite(EN1, HIGH);
+  digitalWrite(EN2, HIGH);
 }
- 
-void loop ()
-{
-  // forward 
-  
-  digitalWrite(pin1,LOW);
-  digitalWrite(pin2, HIGH);
-  digitalWrite(pin3,LOW);
-  digitalWrite(pin4, HIGH);
-  delay(10000);
-  
-    // Backward
-  
-  digitalWrite(pin1,HIGH);
-  digitalWrite(pin2, LOW);
-  digitalWrite(pin3,HIGH);
-  digitalWrite(pin4, LOW);
-  delay(10000);
-  
-  // LEFT TURN
-  
-  
-  digitalWrite(pin1,LOW);
-  digitalWrite(pin2,LOW);
-  digitalWrite(pin3,HIGH);
-  digitalWrite(pin4, LOW);
-  
-  delay(10000);
-  
-   // Right turn 
-  
-  digitalWrite(pin1,HIGH);
-  digitalWrite(pin2, LOW);
-  digitalWrite(pin3,LOW);
-  digitalWrite(pin4, LOW);
-  
-  delay(10000);
-  
-  
-  // stop
-  
-  digitalWrite(pin1,LOW);
-  digitalWrite(pin2, LOW);
-  digitalWrite(pin3,LOW);
-  digitalWrite(pin4, LOW);
-  
-  delay(10000);
-  
 
+void loop() {
+  // FORWARD
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  delay(1500);
+
+  // STOP
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  delay(500);
+
+  // REVERSE
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  delay(1500);
+
+  // STOP
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  delay(500);
 }
